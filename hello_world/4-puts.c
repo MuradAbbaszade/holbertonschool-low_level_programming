8410a848531ea65a9b9a628a8a85dc95f7cc061f
Murad Abbaszade <muradabbaszade6@gmail.com>
2023-10-13 08:09:08 -0700

    a

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-/*print*/
+/**
+ * main - This is the entry point of the code
+ *
+ * Return - 0 Successful
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
