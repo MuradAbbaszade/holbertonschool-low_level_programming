bc52bac02c8ea8ec3e6a179cf8866e2e41b3046a
Murad Abbaszade <muradabbaszade6@gmail.com>
2023-10-15 00:36:08 -0700

    a

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,13 +1,21 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
 
 /**
- * main - Entry point
+ * main - Entry
+ *
+ * Description: 'print with  putchar()'
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
+	char st[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	int n = 0;
+
+	while (st[n] != '\0')
+	{
+		putchar(st[n]);
+		n++;
+	}
 	return (1);
 }
