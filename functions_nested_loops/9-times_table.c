baca43019a22ac7c006d8ed5b6eb115e3547d415
Murad Abbaszade <muradabbaszade6@gmail.com>
2023-10-18 06:50:05 -0700

    a

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * times_table - check the code
+ * void
+ * Return: Always void.
+ */
+void times_table(void)
+{
+int i = 0, j = 0, n = 0, d = 0;
+for (i = 0 ; i <= 9 ; i++)
+{
+for (j = 0 ; j <= 9 ; j++)
+{
+if (n < 10)
+{
+printf(" %d ", n);
+}
+else
+{
+printf("%d ", n);
+}
+n = n + d;
+}
+n = 0;
+d++;
+printf("\n");
+}
+}
