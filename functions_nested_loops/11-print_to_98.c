6ce1ce499929f34656bfe5f2e2fdd18cbe9eea81
Murad Abbaszade <muradabbaszade6@gmail.com>
2023-10-18 07:55:25 -0700

    a

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - check the code
+ * @n:input
+ * Return: Always void.
+ */
+void print_to_98(int n)
+{
+int i;
+if (n < 98)
+{
+for (i = n ; i <= 98 ; i++)
+{
+if (i == 98)
+{
+printf("%d", i);
+}
+else
+{
+printf("%d, ", i);
+}
+}
+}
+else
+{
+for (i = n ; i >= 98 ; i--)
+{
+if (i == 98)
+{
+printf("%d", i);
+}
+else
+{
+printf("%d, ", i);
+}
+}
+}
+}
