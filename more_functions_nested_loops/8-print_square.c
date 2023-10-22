a82e9252d79764ee759afb675ab80c94bd399364
Murad Abbaszade <muradabbaszade6@gmail.com>
2023-10-22 08:12:43 -0700

    a

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_diagonal - check the code
+ * @n:input
+ * Return: Always 0.
+ */
+void print_square(int n)
+{
+int i, j;
+if (n > 0)
+{
+for (i = 0 ; i < n ; i++)
+{
+for (j = 0 ; j < n ; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
+}
