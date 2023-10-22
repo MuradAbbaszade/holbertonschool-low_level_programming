beb97eec83fbcfbefe08382fe0270ab04399502a
Murad Abbaszade <muradabbaszade6@gmail.com>
2023-10-22 11:18:21 -0700

    a

---
+++ more_functions_nested_loops/101-print_comb4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main(void)
+{
+int i, j, k, a = 1, b = 2, y = 2;
+for (i = 0 ; i < 10 ; i++)
+{
+for (j = a ; j < 10 ; j++)
+{
+for (k = b ; k < 10 ; k++)
+{
+putchar(i + '0');
+putchar(j + '0');
+putchar(k + '0');
+if (i != 7)
+{
+putchar(',');
+putchar(' ');
+}
+else
+{
+putchar('\n');
+}
+}
+b++;
+}
+b = ++y;
+a++;
+}
+return (0);
+}
