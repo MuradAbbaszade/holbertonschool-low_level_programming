8d21493c509dd7bdc9a5bfca42ed8dddae423c73
Murad Abbaszade <muradabbaszade6@gmail.com>
2023-10-22 04:40:27 -0700

    a

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * print_numbers - function
+ * void
+ * Return: nothing.
+ */
+int print_numbers(void)
+{
+for (int i = 0 ; i < 10 ; i++)
+{
+putchar(i);
+}
+putchar("/n");
+}
