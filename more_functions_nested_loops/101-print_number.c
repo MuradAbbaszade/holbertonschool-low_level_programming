6a5442587b7c3656308909f496a7e4be185e6e95
Murad Abbaszade <muradabbaszade6@gmail.com>
2023-10-22 10:04:55 -0700

    a

---
+++ more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_number - Entry point
+ *
+ * @n: 'var
+ *
+ * Return: Always 0 (Success)
+ */
+void print_number(int n)
+{
+int i = 1;
+if (n < 0)
+{
+_putchar('-');
+n=-1*n;
+}
+while ((n / i) / 10 != 0)
+{
+i = i * 10;
+}
+while(i != 1)
+{
+_putchar((n / i) + '0');
+n = n % i;
+}
+_putchar((n % 10) + '0');
+ i/=10;
+}
