966d0282d3b93b084248261b100bfd5a6caf73f0
Murad Abbaszade <muradabbaszade6@gmail.com>
2023-10-22 04:08:55 -0700

    a

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "ctype.h"
+/**
+ * _isupper - function.
+ * @C:input
+ * Return: 0 1.
+ */
+int _isupper(char c)
+{
+if (isupper(c))
+return (1);
+else
+return (0);
+}
+
