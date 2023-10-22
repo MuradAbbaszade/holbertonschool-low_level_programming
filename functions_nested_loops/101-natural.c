007b4df4b96e20dae3595bab26adf518c146d37f
Murad Abbaszade <muradabbaszade6@gmail.com>
2023-10-22 11:44:21 -0700

    13

---
+++ functions_nested_loops/101-natural.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main() {
+int i, sum = 0;
+for (i = 1 ; i < 1024 ; i++)
+{
+if (i % 3 == 0 || i % 5 == 0)
+{
+sum += i;
+}
+}
+printf("%d\n", sum);
+}
