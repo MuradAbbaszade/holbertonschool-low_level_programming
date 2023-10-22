a6cbdbb4c73148bf91b7553e1566cd2c14f2f28f
Murad Abbaszade <muradabbaszade6@gmail.com>
2023-10-22 10:17:20 -0700

    a

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -14,7 +14,7 @@ int i = 1;
 if (n < 0)
 {
 _putchar('-');
-n=-1*n;
+n = -1 * n;
 }
 while ((n / i) / 10 != 0)
 {
@@ -24,7 +24,7 @@ while(i != 1)
 {
 _putchar((n / i) + '0');
 n = n % i;
+i /= 10;
 }
 _putchar((n % 10) + '0');
- i/=10;
 }
