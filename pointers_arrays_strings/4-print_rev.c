a188ffffe3ff723d192aa1c8f77744c38e2ee503
Murad Abbaszade <muradabbaszade6@gmail.com>
2023-10-23 06:05:04 -0700

    a

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * print_rev -reverse
+ * @s:input
+ * Return: Always nothing.
+ */
+void print_rev(char *s)
+{
+int len = strlen(s);
+int i;
+for (i = 0 ; i < (len * 2) - 1 ; i++)
+{
+if (i < len - 1) s++;
+else
+_putchar(*s--);
+}
+_putchar('\n');
+}
