b60dca473e117b60df99758e6fdb6f10cf417098
Murad Abbaszade <muradabbaszade6@gmail.com>
2023-10-23 04:13:41 -0700

    a

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _puts - check the code
+ * @str:input
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+while(*str)
+_putchar(*str++);
+_putchar('\n');
+}
