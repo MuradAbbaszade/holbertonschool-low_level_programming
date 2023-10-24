5266c440d8e8b6eda73440d313c9abc90d4ac34c
Murad Abbaszade <muradabbaszade6@gmail.com>
2023-10-24 06:46:07 -0700

    a

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - check the code for
+ * @a:input
+ * @b:input
+ * Return: Always 0.
+ */
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0 ; i < n ; i++)
+{
+if (i == n - 1)
+printf("%d", *a++);
+else
+printf("%d, ", *a++);
+}
+}
