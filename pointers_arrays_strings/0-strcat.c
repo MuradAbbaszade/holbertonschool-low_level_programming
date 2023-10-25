08e995b45fa7a3d248644fe544625264d64b2f2e
Murad Abbaszade <muradabbaszade6@gmail.com>
2023-10-25 09:23:17 -0700

    a

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcat - check the code
+ * @dest:input
+ * @src:input
+ * Return: Always 0.
+ */
+char *_strcat(char *dest, char *src)
+{
+int dest_len = strlen(dest);
+int n = strlen(src);
+int i;
+for (i = 0 ; i < n && src[i] != '\0' ; i++)
+dest[dest_len + i] = src[i];
+dest[dest_len + i] = '\0';
+return dest;
+}
