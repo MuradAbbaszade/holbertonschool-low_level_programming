92f3ced96c3404b5f83e5c4fa012b84f8492afac
Murad Abbaszade <muradabbaszade6@gmail.com>
2023-10-25 05:37:56 -0700

    a

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - check the code
+ * @dest:input
+ * @src:input
+ * Return: Always 0.
+ */
+char *_strcpy(char *dest, const char *src)
+{
+int index = 0;
+while (src[index])
+{
+dest[index] = src[index];
+index++;
+}
+return (dest);
+}
