c51ee0324743f0b7f71fb435daf1a74760494b63
Murad Abbaszade <muradabbaszade6@gmail.com>
2023-10-25 05:49:11 -0700

    a

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -9,11 +9,10 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-int index = 0;
-while (src[index])
-{
-dest[index] = src[index];
-index++;
-}
-return (dest);
+int i;
+for (i = 0; src[i] != '\0'; i++)
+dest[i] = src[i];
+for ( ; src[i] !='\0'; i++)
+dest[i] = '\0';
+return dest;
 }
