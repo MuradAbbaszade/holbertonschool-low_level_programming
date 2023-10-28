d703526ecc4040608ce129fc6d411a2ed93f74dc
Murad Abbaszade <muradabbaszade6@gmail.com>
2023-10-28 13:00:44 -0700

    a

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,17 @@
+/**
+ * strncpy - check the code
+ * @dest:input
+ * @src:input
+ * @n:input
+ * Return: Always 0.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+for (i = 0; i < n && src[i] != '\0'; i++) {
+dest[i] = src[i];
+}
+for (; i < n; i++) {
+dest[i] = '\0';
+}
+}
