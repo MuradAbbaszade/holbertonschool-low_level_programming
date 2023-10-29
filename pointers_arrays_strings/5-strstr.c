e68c0862e0d9e0514a0626cb2022ce86765fd6ef
Murad Abbaszade <muradabbaszade6@gmail.com>
2023-10-29 05:56:15 -0700

    a

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strstr - check the code
+ * @haystack:input
+ * @needle:input
+ * Return: Always 0.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+if (*needle == '\0')
+{
+return (haystack);
+}
+while (*haystack != '\0')
+{
+char *h = haystack;
+char *n = needle;
+while (*n != '\0' && *h == *n)
+{
+h++;
+n++;
+}
+if (*n == '\0')
+{
+return (haystack);
+}
+haystack++;
+}
+return (NULL);
+}
