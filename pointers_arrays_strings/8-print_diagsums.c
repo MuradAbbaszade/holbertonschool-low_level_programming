4217f32b2e37979ec8612e5379c0c230fb9d2b6f
Murad Abbaszade <muradabbaszade6@gmail.com>
2023-10-29 06:57:55 -0700

    a

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -14,11 +14,11 @@ int sum1 = 0, sum2 =  0;
 for (i = 0; i < size; i++)
 for (j = 0 ; j < size ; j++)
 {
-if (i == j) 
+if (i == j)
 sum1 += a[i * size + i];
-if (j == size - 1 - i) 
+if (j == size - 1 - i)
 sum2 += a[i * size + (size - 1 - i)];
-} 
+}
 }
 printf("%d, %d\n", sum1, sum2);
 }
