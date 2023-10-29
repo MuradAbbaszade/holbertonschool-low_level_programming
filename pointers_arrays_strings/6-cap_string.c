5fc53246a906cb95ffb406993572d494e55a2445
Murad Abbaszade <muradabbaszade6@gmail.com>
2023-10-29 03:15:45 -0700

    a

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -14,10 +14,12 @@ int i = 0, len = strlen(str);
 while (i < len)
 {
 c = *(str + i);
-if ((c >= 58 && c <= 63) || (c >= 33 && c <= 44) || c == 46
+if (c >= 97 && c <= 122 && i == 1)
+*(str + i) = c - 32;
+else if ((c >= 58 && c <= 63) || (c >= 33 && c <= 44) || c == 46
 || c == ' ' || c == '\t' || c == '\n')
 isSeperator = 1;
-else if ((c >= 97 && c <= 122 && isSeperator == 1) || i == 0)
+else if (c >= 97 && c <= 122 && isSeperator == 1)
 {
 *(str + i) = c - 32;
 isSeperator = 0;
