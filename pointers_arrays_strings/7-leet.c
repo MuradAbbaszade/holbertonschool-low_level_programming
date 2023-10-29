4005ba56f6820e8fdf4f85eea9620dc8a587cb89
Murad Abbaszade <muradabbaszade6@gmail.com>
2023-10-29 01:42:33 -0700

    a

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * leet - check the code for
+ * @str:input
+ * Return: Always 0.
+ */
+char *leet(char *str)
+{
+int i, j;
+int len = strlen(str);
+char letters[] = {'a', 'e', 'o', 't', 'l'};
+char numbers[] = {'4', '3', '0', '7', '1'};
+for (i = 0 ; i < 5 ; i++)
+{
+for (j = 0 ; j < len ; j++)
+{
+if (str[j] == letters[i] || str[j] == letters[i] - 32)
+*(str+j) = numbers[i];
+}
+}
+return (str);
+}
