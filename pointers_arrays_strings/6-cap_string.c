54accc742a09b08ad96d5435b40bf76c3dfdde80
Murad Abbaszade <muradabbaszade6@gmail.com>
2023-10-29 03:08:31 -0700

    a

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdio.h>
-
+#include <string.h>
 /**
  * cap_string - check the code
  * @str:input
@@ -8,20 +8,23 @@
  */
 char *cap_string(char *str)
 {
+char c;
 int isSeperator = 0;
-while (*str)
+int i = 0, len = strlen(str);
+while (i < len)
 {
-if ((*str >= 58 && *str <= 63) || (*str >= 33 && *str <= 46)
-|| *str == ' ' || *str == '\t' || *str == '\n')
+c = *(str + i);
+if ((c >= 58 && c <= 63) || (c >= 33 && c <= 44) || c == 46
+|| c == ' ' || c == '\t' || c == '\n')
 isSeperator = 1;
-else if (*str >= 97 && *str <= 122 && isSeperator == 1)
+else if (c >= 97 && c <= 122 && isSeperator == 1)
 {
-*str = *str - 32;
+*(str + i) = c - 32;
 isSeperator = 0;
 }
 else
 isSeperator = 0;
-str++;
+i++;
 }
 return (str);
 }
