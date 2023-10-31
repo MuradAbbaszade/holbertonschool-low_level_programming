cb8a839aa51e3ed15b911a4c019d31302254434a
Murad Abbaszade <muradabbaszade6@gmail.com>
2023-10-31 06:02:29 -0700

    a

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
-
+#include "main.h"
 /**
  * _puts_recursion - check the code
  * @s:input
  * Return: Always 0.
  */
-void _puts_recursion(char *s){
+void _puts_recursion(char *s)
+{
 if (*s == '\0')
 {
-return (0);
+_putchar('\n');
+return;
 }
-putchar(*s);
+_putchar(*s);
 s++;
 _puts_recursion(s);
 }
