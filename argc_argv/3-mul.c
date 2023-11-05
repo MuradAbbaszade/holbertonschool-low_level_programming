02317c8d933c2e427ad588ac3367d6f32cb375c0
Murad Abbaszade <muradabbaszade6@gmail.com>
2023-11-05 07:11:27 -0800

    a

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - a
+ * @argc: a
+ * @argv: a
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+int a, b;
+if (argc == 3)
+{
+a = atoi(argv[1]);
+b = atoi(argv[2]);
+printf("%d\n", a * b);
+return (1);
+}
+else
+printf("Error\n");
+return (0);
+}
