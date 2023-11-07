587b5ca5158b45628bdf20eb322c245d0327581a
Murad Abbaszade <muradabbaszade6@gmail.com>
2023-11-07 05:51:26 -0800

    a

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -8,9 +8,14 @@
  */
 char *create_array(unsigned int size, char c)
 {
+char *ar;
+unsigned int i;
 if (size == 0)
 return (NULL);
-char *ar = malloc(size * sizeof(char));
-ar[0] = c;
+ar = malloc(size * sizeof(char));
+for(i = 0 ; i < size ; i++)
+{
+ar[i] = c;
+}
 return (ar);
 }
