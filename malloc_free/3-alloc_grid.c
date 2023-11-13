fb95f2d2cbae8fd4e91859b9c2c19b597ca8619a
Murad Abbaszade <muradabbaszade6@gmail.com>
2023-11-13 09:04:46 -0800

    a

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -15,12 +15,15 @@ return (NULL);
 arr = (int **)malloc(height * sizeof(int *));
 if (arr == NULL)
 {
-for (i = 0; i < height; i++)
-free(arr[i]);
+free(arr);
 return (NULL);
 }
 for (i = 0; i < height; i++)
+{
 arr[i] = (int *)malloc(width * sizeof(int));
+if (arr[i] == NULL)
+return (NULL);
+}
 for (i = 0; i < height; i++)
 for (j = 0; j < width; j++)
 arr[i][j] = 0;
