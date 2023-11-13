55ec772c5c0843ede053a2c02bcb9a9b0fb365e4
Murad Abbaszade <muradabbaszade6@gmail.com>
2023-11-13 07:08:16 -0800

    a

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -10,21 +10,26 @@
 char *str_concat(char *s1, char *s2)
 {
 char *s3;
+int i;
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
-s3 = malloc(sizeof(s1)+sizeof(s2));
-while (!s1)
+s3 = malloc(sizeof(s1) + sizeof(s2));
+if (s3 == NULL)
+return (NULL);
+i = 0;
+while (*s1)
 {
-s3 += *s1;
+s3[i] = *s1;
 s1++;
+i++;
 }
-while (!s2)
+while (*s2)
 {
-s3 += *s2;
+s3[i] = *s2;
 s2++;
+i++;
 }
-*s3 += NULL;
 return (s3);
 }
