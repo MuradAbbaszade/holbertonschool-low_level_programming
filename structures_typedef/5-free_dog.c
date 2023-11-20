25d458c1a5d521bdd002babdbad9e89da5340697
Murad Abbaszade <muradabbaszade6@gmail.com>
2023-11-20 05:14:29 -0800

    a

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - check the code
+ * @d:d
+ * Return: Always 0.
+ */
+void free_dog(dog_t *d)
+{
+free(d->name);
+free(d->owner);
+free(d);
+}
