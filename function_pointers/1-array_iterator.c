7650d29e5d45152d7391b84b1f77f2c24988e610
Murad Abbaszade <muradabbaszade6@gmail.com>
2023-11-26 07:21:26 -0800

    a

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * array_iterator - prints an integer
+ * @array: the integer to print
+ * @size:size
+ * @action:action
+ * Return: Nothing.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+unsigned int i;
+if (action == NULL)
+return;
+for (i = 0 ; i < size ; i++)
+action(array[i]);
+}
