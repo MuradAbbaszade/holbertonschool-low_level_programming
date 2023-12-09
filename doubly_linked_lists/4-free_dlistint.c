934ab4ed73df67725060fd0c0cb44e4957d4d776
Murad Abbaszade <muradabbaszade6@gmail.com>
2023-12-09 05:15:03 -0800

    a

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_dlistint - check the code
+ * @head:head
+ * Return: Always 0.
+*/
+void free_dlistint(dlistint_t *head)
+{
+dlistint_t *temp;
+temp = head;
+while(head != NULL)
+{
+temp = head;
+head = head->next;
+free(temp);
+}
+}
