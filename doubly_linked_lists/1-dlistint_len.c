f60baea23abfb1cb68e5c694b5294886f94765db
Murad Abbaszade <muradabbaszade6@gmail.com>
2023-12-09 04:51:57 -0800

    a

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * print_dlistint - check the code
+ * dlistint_len - check the code
  * @h:n
  * Return: Always 0.
 */
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 unsigned int result = 0;
 while (h != NULL)
