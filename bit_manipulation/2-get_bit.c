8a206d08ee2e73b1e6ee83ca2458d30a6bad0083
Murad Abbaszade <muradabbaszade6@gmail.com>
2023-12-10 06:56:08 -0800

    a

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -9,11 +9,13 @@
 */
 int get_bit(unsigned long int n, unsigned int index)
 {
+unsigned long int a;
+int bit;
 if (index >= sizeof(unsigned long int) * 8)
 {
 return (-1);
 }
-unsigned long int a = n >> index;
-int bit = a & 1;
+a = n >> index;
+bit = a & 1;
 return (bit);
 }
