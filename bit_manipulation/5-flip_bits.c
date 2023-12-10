33ddbc07dec2310e047cf2254a3361bde15d5948
Murad Abbaszade <muradabbaszade6@gmail.com>
2023-12-10 07:29:50 -0800

    a

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -2,21 +2,25 @@
 #include <string.h>
 #include "main.h"
 /**
- * flip_bits - check the code
+ * count_set_bits - check the code
  * @n:b
- * @m:index
  * Return: Always 0.
 */
 unsigned int count_set_bits(unsigned long int n)
 {
 unsigned int count = 0;
-while (n) {
+while (n)
+{
 count += n & 1;
 n >>= 1;
 }
 return (count);
 }
-
+/**
+ * flip_bits - a
+ * @n:n
+ * @m:m
+*/
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 unsigned long int xor_result = n ^ m;
