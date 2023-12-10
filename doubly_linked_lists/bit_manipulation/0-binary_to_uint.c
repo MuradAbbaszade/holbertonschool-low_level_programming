9844a827aa4975ecaafeba42d8fa384d7053568d
Murad Abbaszade <muradabbaszade6@gmail.com>
2023-12-10 05:54:33 -0800

    a

---
+++ doubly_linked_lists/bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+unsigned int binary_to_uint(const char *b)
+{
+unsigned int result;
+int i = 0;
+int len = strlen(b);
+int j = 0;
+int pow = 1;
+if (b == NULL)
+return (0);
+for (i = len - 1 ; ((i != len - 10) && (i != -1)) ; i--)
+{
+if (b[i] != '0' && b[i] != '1')
+return (0);
+else if (b[i] == '1')
+{
+pow = 1;
+for (j = 0 ; (j < (len - i - 1)) ; j++)
+{
+pow *= 2;
+}
+result += pow;
+}
+}
+return (result);
+}
