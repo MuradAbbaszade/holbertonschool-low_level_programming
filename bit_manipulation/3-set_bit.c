0a4732553449c128d7c6c193500cb17f19cca0ae
Murad Abbaszade <muradabbaszade6@gmail.com>
2023-12-10 07:06:54 -0800

    a

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * Set_bit - check the code
+ * @n:b
+ * @index:index
+ * Return: Always 0.
+*/
+int set_bit(unsigned long int *n, unsigned int index) {
+if (index >= sizeof(unsigned long int) * 8) {
+return (-1);
+}
+*n |= (1 << index);
+return (1);
+}
