d445f6aaf1424b7732b7dae6d3648ac3ca76238a
root <muradabbaszade6@gmail.com>
2023-12-17 06:48:39 -0800

    a

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -13,7 +13,7 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 int len;
 int close_val;
-int file; 
+int file;
 int i;
 file = open(filename, O_WRONLY | O_APPEND);
 if (text_content == NULL)
